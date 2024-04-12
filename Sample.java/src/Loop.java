961e39ab4ce8761c2d79d1c74745d2fb5fb2df91
Administrator <Administrator@k7>
2024-04-12 19:26:52 +0530

    Pushing sample.java to git hub

---
+++ Sample.java/src/Loop.java
@@ -0,0 +1,20 @@
+
+public class Loop {
+
+	public static void main(String[] args) 
+	{
+		for(int row=1; row<=4; row++)
+		{
+			for (int col=3; col>=row; col--)
+			{
+				
+				System.out.print(row);
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
